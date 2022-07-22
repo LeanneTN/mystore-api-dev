0ed6a6a47836d76ea92fec74b4a7bdfd7fbdf3be
LeanneTN <2321604632@qq.com>
2022-07-22 15:06:53 +0800

    init project

---
+++ src/test/java/com/example/mystoreapidev/MystoreApiDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mystoreapidev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MystoreApiDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
