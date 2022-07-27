edb0c0407f45022390f624f0ca5c3f72bc74cb05
LeanneTN <2321604632@qq.com>
2022-07-27 15:08:51 +0800

    small bug fix

--- src/main/java/com/example/mystoreapidev/domain/User.java
+++ src/main/java/com/example/mystoreapidev/domain/User.java
@@ -1,18 +1,27 @@
 package com.example.mystoreapidev.domain;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import javafx.util.converter.LocalDateStringConverter;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
 @Data
 @TableName("mystore_user")
 public class User {
+    @TableId(type = IdType.AUTO)
     private Integer id;
+    @NotBlank(message = "username can't be empty")
     private String username;
+    @NotBlank(message = "password can't be empty")
     private String password;
+    @NotBlank(message = "email can't be empty")
     private String email;
+    @NotBlank(message = "phone can't be empty")
     private String phone;
 
     private String question;
@@ -22,6 +31,11 @@ public class User {
 
     //localDateTime fixes original questions from Date
     //most extinguish shortcoming of Date is threads of Date is less secure
+    //mybatis-plus can automatically render two similar name between POJO and database but the capability is limited
+    //like: createTime -> create_time
+    //use @TableField to avoid error that might happen
+    @TableField("create_time")
     private LocalDateTime createTime;
+    @TableField("update_time")
     private LocalDateTime updateTime;
 }
