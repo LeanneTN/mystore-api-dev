03f2e0da124093ed1280f67fc2bfcbc481190eb0
LeanneTN <2321604632@qq.com>
2022-08-04 23:42:59 +0800

    new git ignore policy

--- src/main/java/com/example/mystoreapidev/controller/admin/AliOSSController.java
+++ src/main/java/com/example/mystoreapidev/controller/admin/AliOSSController.java
@@ -1,14 +1,22 @@
 package com.example.mystoreapidev.controller.admin;
 
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.common.utils.BinaryUtil;
+import com.aliyun.oss.model.MatchMode;
+import com.aliyun.oss.model.PolicyConditions;
 import com.example.mystoreapidev.VO.AliOSSPolicy;
 import com.example.mystoreapidev.common.CommonResponse;
 import com.example.mystoreapidev.service.OSSService;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
 
 @RestController
 @RequestMapping("/alioss/")
@@ -18,25 +26,10 @@ public class AliOSSController {
     @Autowired
     private OSSService ossService;
 
-    @Value("${aliyun.oss.accessId}")
-    private String ACCESS_ID;
-    @Value("${aliyun.oss.accessKey}")
-    private String ACCESS_KEY;
-    @Value("${aliyun.oss.endpoint")
-    private String ENDPOINT;
-    @Value("${aliyun.oss.bucket")
-    private String BUCKET;
-    @Value("${aliyun.oss.dir}")
-    private String DIR;
-
     @GetMapping("get_policy")
     public CommonResponse<AliOSSPolicy> getPolicy(){
-        String host = "http://"+BUCKET+"."+ENDPOINT;
-        try{
 
-        }catch (Exception e){
-            log.info("server end generate alioss policy failed.",e);
-        }
+
         return ossService.generatePolicy();
     }
 }
--- src/main/java/com/example/mystoreapidev/service/impl/AliOSSServiceImpl.java
+++ src/main/java/com/example/mystoreapidev/service/impl/AliOSSServiceImpl.java
@@ -1,22 +1,61 @@
 package com.example.mystoreapidev.service.impl;
 
 import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.common.utils.BinaryUtil;
+import com.aliyun.oss.model.MatchMode;
+import com.aliyun.oss.model.PolicyConditions;
 import com.example.mystoreapidev.VO.AliOSSPolicy;
 import com.example.mystoreapidev.common.CommonResponse;
 import com.example.mystoreapidev.service.OSSService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
 @Service("ossService")
+@Slf4j
 public class AliOSSServiceImpl implements OSSService {
 
     @Autowired
     private OSSClient ossClient;
 
+    @Value("${aliyun.oss.accessId}")
+    private String ACCESS_ID;
+    @Value("${aliyun.oss.accessKey}")
+    private String ACCESS_KEY;
+    @Value("${aliyun.oss.endpoint")
+    private String ENDPOINT;
+    @Value("${aliyun.oss.bucket")
+    private String BUCKET;
+    @Value("${aliyun.oss.dir}")
+    private String DIR;
+
+    @Value("${aliyun.oss.policy.expire}")
+    private String EXPIRE;
+    @Value("${aliyun.oss.policy.maxSize}")
+    private long MAX_SIZE;
+
     @Override
     public CommonResponse<AliOSSPolicy> generatePolicy() {
-        System.out.println("aaa");
-        System.out.println(ossClient.getBucketInfo("mystore-demo").getBucket().getLocation());
+        String host = "http://"+BUCKET+"."+ENDPOINT;
+        try{
+            long expireTime = 30;
+            long expireEndTime = System.currentTimeMillis() + expireTime * 1000;
+            Date expiration = new Date(expireEndTime);
+            PolicyConditions policyConditions = new PolicyConditions();
+            policyConditions.addConditionItem(PolicyConditions.COND_CONTENT_LENGTH_RANGE, 0, 1024*1024*MAX_SIZE);
+            policyConditions.addConditionItem(MatchMode.StartWith, PolicyConditions.COND_KEY, DIR);
+
+            String postPolicy = ossClient.generatePostPolicy(expiration, policyConditions);
+            byte[] binaryData = postPolicy.getBytes(StandardCharsets.UTF_8);
+            String encodedPolicy = BinaryUtil.toBase64String(binaryData);
+            String postSignature = ossClient.calculatePostSignature(postPolicy);
+        }catch (Exception e){
+            log.info("server end generate alioss policy failed.",e);
+        }
         return null;
     }
 }
