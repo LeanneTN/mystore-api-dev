84e216b08d5479227ef22daed897068929f77f15
LeanneTN <2321604632@qq.com>
2022-09-25 17:27:18 +0800

    chore: some annotation

--- src/main/java/com/example/mystoreapidev/controller/CORSFilter.java
+++ src/main/java/com/example/mystoreapidev/controller/CORSFilter.java
@@ -1,10 +1,15 @@
 package com.example.mystoreapidev.controller;
 
+import org.springframework.context.annotation.Configuration;
+
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Configuration
+@WebFilter(filterName = "CORSFilter")
 public class CORSFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
