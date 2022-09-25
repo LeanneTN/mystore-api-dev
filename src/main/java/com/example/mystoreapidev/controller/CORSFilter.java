6a8dd2d3a4ee25fa6cec453b997e69310d16dd67
LeanneTN <2321604632@qq.com>
2022-09-25 17:25:26 +0800

    perf(controller): cross origin resource controller

---
+++ src/main/java/com/example/mystoreapidev/controller/CORSFilter.java
@@ -0,0 +1,22 @@
+package com.example.mystoreapidev.controller;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CORSFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE, PUT");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+        filterChain.doFilter(request, response);
+    }
+}
--- src/main/java/com/example/mystoreapidev/controller/front/ProductController.java
+++ src/main/java/com/example/mystoreapidev/controller/front/ProductController.java
@@ -8,16 +8,14 @@ import com.example.mystoreapidev.domain.Product;
 import com.example.mystoreapidev.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.NotNull;
 
 @RestController
 @RequestMapping("/product/")
 @Validated
+@CrossOrigin
 public class ProductController {
     @Autowired
     private ProductService productService;
